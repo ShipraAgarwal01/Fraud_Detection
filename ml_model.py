16f39504e62e815f300e23bdcf2d13bfdbb52bf1
shipraagarwal01 <shipra.agarwal.mca@gmail.com>
2022-05-03 08:01:42 -0500

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 
 
 print("adding all ml realted code here")
+
+
+def model1():
+    print("adding code for l model")
+
+def model2():
+    print("adding code for 2 model")
+
+def model3():
+    print("adding code for 3 model")
